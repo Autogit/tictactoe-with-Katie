a108cb2c6919d20fad4c2f806a59068141dafa6f
Matt Taylor <Matt@Navgear.com>
2014-09-03 15:34:20 -0500

    this is perfect!!!

--- tic-tac-toe/ttt.js
+++ tic-tac-toe/ttt.js
@@ -5,51 +5,57 @@ window.onload = function(){
   
   var timer = 0;
   var turn = "";
+  var catsTimer = 0;
   
   var add_mark = function(){
     if(timer % 2 == 0){
       var turn = "X";
       this.innerHTML = turn;      
       timer++;
+      catsTimer++;
     }else if(timer % 2 == 1){
       var turn = "O";
       this.innerHTML = turn;
       timer++;
+      catsTimer++;
     }
+    win();
   }
+  var i = 0;
   
  function win() {
     if(
-      squares[0] == "X" && squares[1] ==  "X" && squares[2] == "X" ||
-      squares[3]  == "X" && squares[4]  == "X" && squares[5] == "X" ||
-      squares[6]  == "X" && squares[7]  == "X" && squares[8] == "X" ||
-      squares[0]  == "X" && squares[3]  == "X" && squares[6] == "X" ||
-      squares[1]  == "X" && squares[4]  == "X" && squares[7] == "X" ||
-      squares[2]  == "X" && squares[5]  == "X" && squares[8] == "X" ||
-      squares[0]  == "X" && squares[4]  == "X" && squares[8] == "X" ||
-      squares[2]  == "X" && squares[4]  == "X" && squares[6] == "X") {
+      (squares[0].innerHTML == "X" && squares[1].innerHTML == "X" && squares[2].innerHTML == "X") ||
+      (squares[3].innerHTML == "X" && squares[4].innerHTML == "X" && squares[5].innerHTML == "X") ||
+      (squares[6].innerHTML == "X" && squares[7].innerHTML == "X" && squares[8].innerHTML == "X") ||
+      (squares[0].innerHTML == "X" && squares[3].innerHTML == "X" && squares[6].innerHTML == "X") ||
+      (squares[1].innerHTML == "X" && squares[4].innerHTML == "X" && squares[7].innerHTML == "X") ||
+      (squares[2].innerHTML == "X" && squares[5].innerHTML == "X" && squares[8].innerHTML == "X") ||
+      (squares[0].innerHTML == "X" && squares[4].innerHTML == "X" && squares[8].innerHTML == "X") ||
+      (squares[2].innerHTML == "X" && squares[4].innerHTML == "X" && squares[6].innerHTML == "X")) {
       console.log("We have a winner!");
       i = 10;
     } else if(
-      squares[0] == "O" && squares[1] ==  "O" && squares[2] == "O" ||
-      squares[3]  == "O" && squares[4]  == "O" && squares[5] == "O" ||
-      squares[6]  == "O" && squares[7]  == "O" && squares[8] == "O" ||
-      squares[0]  == "O" && squares[3]  == "O" && squares[6] == "O" ||
-      squares[1]  == "O" && squares[4]  == "O" && squares[7] == "O" ||
-      squares[2]  == "O" && squares[5]  == "O" && squares[8] == "O" ||
-      squares[0]  == "O" && squares[4]  == "O" && squares[8] == "O" ||
-      squares[2]  == "O" && squares[4]  == "O" && squares[6] == "O") {
+      (squares[0].innerHTML == "O" && squares[1].innerHTML == "O" && squares[2].innerHTML == "O") ||
+      (squares[3].innerHTML == "O" && squares[4].innerHTML == "O" && squares[5].innerHTML == "O") ||
+      (squares[6].innerHTML == "O" && squares[7].innerHTML == "O" && squares[8].innerHTML == "O") ||
+      (squares[0].innerHTML == "O" && squares[3].innerHTML == "O" && squares[6].innerHTML == "O") ||
+      (squares[1].innerHTML == "O" && squares[4].innerHTML == "O" && squares[7].innerHTML == "O") ||
+      (squares[2].innerHTML == "O" && squares[5].innerHTML == "O" && squares[8].innerHTML == "O") ||
+      (squares[0].innerHTML == "O" && squares[4].innerHTML == "O" && squares[8].innerHTML == "O") ||
+      (squares[2].innerHTML == "O" && squares[4].innerHTML == "O" && squares[6].innerHTML == "O")) {
       console.log("We have a winner!");
       i = 10
     } else {
-      console.log("Cats Game");
+        if(catsTimer == 9){  
+          console.log("Cat's Game");
+        }
     }
   }
   
-  for(var i = 0; i < squares.length; i++) {
+  for(i = 0; i < squares.length; i++) {
     // When you click a square, runs the `add_mark` method.
     squares[i].addEventListener("click", add_mark);
-    win();
   }
 
 }
